9419c294a66ca7817b0db47a007600b33254dec3
sbkr74 <sbkr74@gmail.com>
2025-01-30 20:07:10 +0530

    update1.0[logic added]

--- flaskAPI/project/Math_quiz/app.py
+++ flaskAPI/project/Math_quiz/app.py
@@ -14,15 +14,15 @@ def submit():
         x1 = request.form.get("x1", type=float)  # Convert to float (or int if you want)
         x2 = request.form.get("x2", type=float)
 
-        # Validation: Check if both values are numbers and not empty
-        if x1 is None or x2 is None:
-            return "Error: Missing input values!", 400  # Bad Request
+        ans1 = 3
+        ans2 = 2
 
-        # Additional check (example: both values must be positive)
-        if x1 <= 0 or x2 <= 0:
-            return "Error: Both numbers must be positive!", 400
+        if (x1 == ans1 or x1 == ans2) and (x2 == ans1 or x2 == ans2) and (x1!=x2):
+            return f"Correct: {x1} and {x2} "
+        
+        else:
+            return f"incorrect:{x1} or {x2}"
 
-        return f"Received values: x1 = {x1}, x2 = {x2}"
 
     except ValueError:
         return "Invalid input! Please enter numbers.", 400
