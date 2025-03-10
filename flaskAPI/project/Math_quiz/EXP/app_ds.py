9636c02c22633ae27588d52ca759c059dd557a9f
sbkr74 <sbkr74@gmail.com>
2025-03-10 20:07:14 +0530

    update1.0[removed debug codes]

--- flaskAPI/project/Math_quiz/EXP/app_ds.py
+++ flaskAPI/project/Math_quiz/EXP/app_ds.py
@@ -59,27 +59,18 @@ def get_question():
     if 'asked_questions' not in session:
         session['asked_questions'] = []  # Track asked questions
     
-    # Debug: Print session data
-    print("Session asked_questions:", session['asked_questions'])
-    print(ans)
     # Select a random question that hasn't been asked
     available_questions = [q for q in questions if q['id'] not in session['asked_questions']]
     
-    # Debug: Print available questions
-    print("Available questions:", [q['id'] for q in available_questions])
     
     if not available_questions:
         return f"Quiz completed! Your final score is {session['score']}/{len(questions)}"
 
     selected_question = random.choice(available_questions)
     session['asked_questions'].append(selected_question['id'])
-    session.modified = True  # Ensure the session is saved
+    session.modified = True  # Ensure the session is saved   
     
-    # Debug: Print selected question
-    print("Selected question ID:", selected_question['id'])
-    
-    
-    return render_template("index.html", question=selected_question, score=session['score'],ans=ans)
+    return render_template("index.html", question=selected_question, score=session['score'])
 
 @app.route('/submit', methods=['POST'])
 def submit():
