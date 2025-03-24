7740b231e55626a91a86093e5e246f5498374e34
sbkr74 <sbkr74@gmail.com>
2025-03-24 11:52:58 +0530

    check_table.py[debugging] file added

---
+++ flaskAPI/project/Competition_part3/check_table.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+DATABASE = r"flaskAPI/project/Competition_part3/files/quiz.db"
+
+conn = sqlite3.connect(DATABASE)
+cursor = conn.cursor()
+
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+table = cursor.fetchone()
+print(table)
+if table:
+    print("Table 'questions' exists.")
+else:
+    print("Table 'questions' does NOT exist.")
+
+conn.close()
