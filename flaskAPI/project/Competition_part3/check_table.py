a7426b552ca6479047e50b905cd4903d150a0263
sbkr74 <sbkr74@gmail.com>
2025-03-25 07:52:24 +0530

    update2.0 [fetch_question()]

--- flaskAPI/project/Competition_part3/check_table.py
+++ flaskAPI/project/Competition_part3/check_table.py
@@ -2,15 +2,30 @@ import sqlite3
 
 DATABASE = r"flaskAPI/project/Competition_part3/files/quiz.db"
 
-conn = sqlite3.connect(DATABASE)
-cursor = conn.cursor()
+def get_db():
+    conn = sqlite3.connect(DATABASE)
+    # cursor = conn.cursor()
+    conn.row_factory = sqlite3.Row  # Enables dictionary-like row access
+    return conn
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-table = cursor.fetchall()
-print(table)
-if table:
-    print("Table 'questions' exists.")
-else:
-    print("Table 'questions' does NOT exist.")
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# table = cursor.fetchall()
+# print(table)
+# if table:
+#     print("Table 'questions' exists.")
+# else:
+#     print("Table 'questions' does NOT exist.")
 
-conn.close()
+def fetch_question():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM questions;")
+    questions = cursor.fetchall()
+    conn.close()
+    return [dict(q) for q in questions]
+
+que = fetch_question()
+print(que)
+
+
+# conn.close()
